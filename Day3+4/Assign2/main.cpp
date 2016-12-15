5c7166ee270b5c1c90d515a2b8c1c9de15c82091
Finn Boire <finnegan.mccool@gmail.com>
2016-12-14 21:18:25 -0800

    day 3+4 assign2

---
+++ Day3+4/Assign2/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "robot.h"
+
+int main() {
+	bob::robot bob;
+	bob.PickUpPiece();
+	std::cout << bob.GetHasPiece() << std::endl;
+	bob.Drive(6);
+	std::cout << bob.GetPosition() << std::endl;
+	bob.MoveArmTo(10);
+	std::cout << bob.GetArmPosition() << std::endl;
+	bob.ScorePiece();
+	std::cout << bob.GetScore() << std::endl;
+}
---
+++ Day3+4/Assign2/robot.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include "robot.h"
+
+int bob::robot::Drive(int distance) {
+	robotPosition = robotPosition + distance;
+}
+
+int bob::robot::MoveArmTo(int position) {
+	armPosition = position;
+}
+
+int bob::robot::ScorePiece() {
+	if((robotPosition == 6) && (armPosition == 10) && (hasPiece == true)) {
+		score += 1;
+		hasPiece = false;
+	}
+}
+
+int bob::robot::PickUpPiece() {
+	if((armPosition == 0) && (hasPiece == false)) {
+		hasPiece = true;
+	}
+}
+
+bool bob::robot::GetHasPiece() const {
+	return hasPiece;
+}
+
+int bob::robot::GetPosition() const {
+	return robotPosition;
+}
+
+int bob::robot::GetArmPosition() const {
+	return armPosition;
+}
+
+int bob::robot::GetScore() const {
+	return score;
+}
