364cb116681ac4912134fc9f0c1dc84a508fa721
Wentao Chen <wtchen2002@gmail.com>
2016-12-15 01:54:49 +0000

    moved things

---
+++ Day3+4/Assign1/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+	int count = 0;
+
+	std::cout << "How many numbers would you like to enter?" << std::endl;
+	std::cin >> count;
+
+
+	std::vector<int> numberlist;
+
+
+	for (int i = 1; i <= count; i++) {
+		int inputnumber = 0;
+		std::cout << "Enter the " << i << " number" << std::endl;
+		std::cin >> inputnumber;
+		numberlist.push_back(inputnumber);
+	}
+
+	std::sort(numberlist.begin(), numberlist.end());
+
+	for (int i = 0; i < count; i++) {
+    	std::cout << numberlist[i] << " ";
+  	}
+
+  	std:: cout << "" << std::endl;
+
+	return 0;
+}
