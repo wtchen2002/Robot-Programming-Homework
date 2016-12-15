39f224a6915a558f6c1d741b387fe7ff23ac8f80
Finn Boire <finnegan.mccool@gmail.com>
2016-12-14 21:18:25 -0800

    day 3+4 assign 2

---
+++ Day3+4/Assign2/robot.h
@@ -0,0 +1,27 @@
+﻿#ifndef ROBOT_H_
+#define ROBOT_H_
+
+namespace bob {
+
+class robot {
+public:
+	int Drive(int distance);
+	int MoveArmTo(int position);
+	int ScorePiece();
+	int PickUpPiece();
+
+	bool GetHasPiece() const;
+	int GetPosition() const;
+	int GetArmPosition() const;
+	int GetScore() const;
+
+private:
+	int robotPosition = 0;
+	int armPosition = 0;
+	int score = 0;
+	bool hasPiece = false;
+};
+
+}
+
+#endif
