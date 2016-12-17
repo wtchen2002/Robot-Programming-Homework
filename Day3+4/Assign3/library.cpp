3750ee368622756be821a0517874c405a32dd5a7
Finn Boire <finnegan.mccool@gmail.com>
2016-12-16 18:00:20 -0800

    worked on assign3

--- Day3+4/Assign3/library.cpp
+++ Day3+4/Assign3/library.cpp
@@ -7,3 +7,34 @@ void Library::AddPatron(std::string name) {
   Patron.name_ = name
   patrons_.push_back(test)
 }
+
+std::vector<int> Library::GetDate() {
+  std::vector<int> date;
+  int month = 0;
+  int day = 0;
+  int year = 0;
+  std::cout << "Please enter the current month" << std::end1;
+  std::cin >> month;
+  vector.push_back(month);
+  std::cout << "Please enter the current day" << std::end1;
+  std::cin >> day;
+  vector.push_back(day);
+  std::cout << "Please enter the current year" << std::end1;
+  std::cin >> year;
+  vector.push_back(year);
+  return date;
+}
+
+void Library::CheckOutBook(Patron patron, std::string book_name) {
+  bool book_exists = false;
+  for(Book i:books_){
+    if(i.name_ == book_name){
+      book_exists = true; 
+    }
+  }
+  
+  if(book_exists) && (i.in_){
+    i.in_ = false;
+    
+  }
+}
--- Day3+4/Assign3/library.h
+++ Day3+4/Assign3/library.h
@@ -8,21 +8,24 @@ class Library {
    public:
     void SetIn(bool);
     void SetDate(std::vector<int>);
-    bool in_;
+    std::string name_;
+    bool in_ = true;
     std::vector<int> date_out_; // month, day, year
     std::vector<int> date_due_; // month, day, year
   };
 
   class Patron {
    public:
-    std::vector<Book> book_out_;
+    std::vector<Book> books_out_;
     std::string name_;
   };
 
+  std::vector<Book> books_;
   std::vector<Patron> patrons_;
 
+  std::vector<int> GetDate();
   void AddPatron(std::string);
-  void CheckOutBook(Patron, Book);
+  void CheckOutBook(Patron, std::string);
   Book search_for_book(std::string);
   bool check_if_overdue(Book);
 };
