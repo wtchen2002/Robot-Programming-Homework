d6514a3ecc8f402ec5a9182c65a057d661e1910b
Kelly Ostrom <tahoekeo@gmail.com>
2016-12-27 15:52:14 -0800

    started day5 assign3

---
+++ Day5/Assign3/Assign2/main/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "Day5/Assign3/Assign2/robot-lib/robot.h"
+
+int main() {
+	bob::robot bob;
+	bob.PickUpPiece();
+	std::cout << bob.GetHasPiece() << std::endl;
+	bob.Drive(6);
+	std::cout << bob.GetPosition() << std::endl;
+	bob.MoveArmTo(10);
+	std::cout << bob.GetArmPosition() << std::endl;
+	bob.ScorePiece();
+	std::cout << bob.GetScore() << std::endl;
+}
---
+++ Day5/Assign3/Assign2/robot-lib/robot.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include "robot.h"
+
+namespace bob {
+
+int robot::Drive(int distance) {
+	robotPosition = robotPosition + distance;
+}
+
+int robot::MoveArmTo(int position) {
+	armPosition = position;
+}
+
+int robot::ScorePiece() {
+	if((robotPosition == 6) && (armPosition == 10) && (hasPiece == true)) {
+		score += 1;
+		hasPiece = false;
+	}
+}
+
+int robot::PickUpPiece() {
+	if((armPosition == 0) && (hasPiece == false)) {
+		hasPiece = true;
+	}
+}
+
+bool robot::GetHasPiece() const {
+	return hasPiece;
+}
+
+int robot::GetPosition() const {
+	return robotPosition;
+}
+
+int robot::GetArmPosition() const {
+	return armPosition;
+}
+
+int robot::GetScore() const {
+	return score;
+}
+
+}
