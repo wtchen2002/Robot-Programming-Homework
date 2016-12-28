b60ef522442cda3cd3cbdb26a1667f042f1f8197
Kelly Ostrom <tahoekeo@gmail.com>
2016-12-27 16:36:16 -0800

    started day3+4 assign4

---
+++ Day3+4/Assign4/math.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <array>
+
+namespace math {
+
+double Cap(double, double, double);
+std::array<double> CapArray(std::array<double>, double, double);
+double Average(std::array<double>);
+bool Sign(double);
+double Median(std::array<double>);
+double Mode(std::array<double>);
+std::array Difference(std::array<double>, std::array<double>);
+
+};
